5f9a3a2b6bce1195b3bdb1a11894c4ae0edc9ad0
Aswinkumar-J <230701044@rajalakshmi.edu.in>
2025-10-31 21:08:26 +0530

    Create DisplayProduct.js

---
+++ src/Employee/DisplayProduct.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const DisplayProduct = () => {
+  return (
+    <div>
+      <h1>Eyewear Catalog</h1>
+
+      <div>
+        <button>Logout</button>
+      </div>
+
+      <div>
+        <button>Asc</button>
+        <button>Desc</button>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayProduct;
